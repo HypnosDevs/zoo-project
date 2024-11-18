9dc9d96e1e2fb8d8e563fbdac286fd82cd58d14d
HypnosDevs <130528462+HypnosDevs@users.noreply.github.com>
2024-11-18 19:17:00 +0000

    update zoo test

--- test_zoo.py
+++ test_zoo.py
@@ -7,9 +7,9 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(-1), -1)
-        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
         self.assertEqual(self.zoo.get_ticket_price(20), 100)
-        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
         self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
        
